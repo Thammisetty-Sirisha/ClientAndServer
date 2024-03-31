c263c8a5ca42b2799d93baf2f3f986e51af91f01
Thammisetty Sirisha <109898591+Thammisetty-Sirisha@users.noreply.github.com>
2024-03-31 19:13:10 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,24 @@
+var http = require('http'); 
+var options = {
+    hostname: '127.0.0.1',
+    port: 4000, 
+    method:'POST'
+};
+
+var req = http.request(options, function(response){ 
+    var str=''
+    response.on('data',function(chunk){
+        str+=chunk;
+    });
+
+    response.on('end', function(){
+        console.log(str);
+   });
+
+});
+
+req.on('error', error=>{
+    console.error(error);
+});
+
+req.end();
